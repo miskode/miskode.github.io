380846b05adbde264ef00a9a0dad04c2fe83d49d
Miskode <156457880+miskode@users.noreply.github.com>
2024-12-02 19:08:28 -0700

    Start button now updates settings

--- pipe/script.js
+++ pipe/script.js
@@ -1,8 +1,8 @@
 const pipe = new Audio("pipe.mp3");
 pipe.volume = parseInt(document.getElementById("vol").value) / 4;
 
-let min = document.getElementById("min").value * 60000;
-let max = document.getElementById("max").value * 60000;
+let min = Math.abs(document.getElementById("min").value * 60000);
+let max = Math.abs(document.getElementById("max").value * 60000);
 if (min > max) {
     max = min;
     document.getElementById("max").value = document.getElementById("min");
@@ -13,6 +13,13 @@ function randint(x, y) {
 }
 
 function drop() {
+    pipe.volume = parseInt(document.getElementById("vol").value) / 4;
+    let min = Math.abs(document.getElementById("min").value * 60000);
+    let max = Math.abs(document.getElementById("max").value * 60000);
+    if (min > max) {
+        max = min;
+        document.getElementById("max").value = document.getElementById("min");
+    }
     setTimeout(() => {
         drop();
     }, randint(min, max));
